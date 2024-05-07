07843d0ad07cf0e2d594b42fccfcb886613629cf
Khalil-Elemam <khalil2020100@gmail.com>
2024-05-07 09:39:21 +0300

    The end

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:Khalil100100@localhost/cloudy_members"
+)
+db = SQLAlchemy(app)
 
-# Not implemented
+
+class Team(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    CGPA = db.Column(db.DECIMAL, nullable=False)
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# Not implemented
 @app.route("/team")
 def team():
-    return render_template("team.html")
+    return render_template("team.html", team=Team.query.all())
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
